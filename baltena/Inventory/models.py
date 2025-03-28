378ce82b20d3efcd3950fb8cebe51ae4dc677966
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-03-28 09:26:34 +0300

    models.py tables have been finilized, migrations were done and connected to mysql database

--- baltena/Inventory/models.py
+++ baltena/Inventory/models.py
@@ -1,59 +1,37 @@
 from django.db import models
 
 class Category(models.Model):
-    category_name = models.Charfield(max_length=100)
+    category_name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
 
     def __str__(self):
         return self.category_name
     
 class Item(models.Model):
-    item_name = models.Charfield(max_length=100)
+    item_name = models.CharField(max_length=100)
     description = models.TextField()
     base_price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
 
     def __str__(self):
-        return self.description
-
-class PriceHistory(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='price_history')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    effective_date = models.DateField()
-
-    def __str__(self):
-        return f"{self.item.description} - {self.price} on {self.effective_date}"
-
-
-class IncomingItems(models.Model):
-    inventory_id = models.AutoField(primary_key=True)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='incoming_items')
-    size = models.ForeignKey('Size', on_delete=models.CASCADE, related_name='incoming_items')
-    quantity_received = models.PositiveIntegerField()
-
-    def __str__(self):
-        return f"{self.quantity_received} of {self.item.description}"
-
+        return self.item_name
 
 class Size(models.Model):
-    description = models.CharField(max_length=50)
+    size_name = models.CharField(max_length=50)
+    item = models.ForeignKey(Item,on_delete=models.CASCADE, related_name='size')
 
     def __str__(self):
-        return self.description
+        return self.size_name
 
-
-class Orders(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    customer = models.ForeignKey('Customer', on_delete=models.CASCADE, related_name='orders')
-    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='orders')
-    quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.ForeignKey('Status', on_delete=models.CASCADE, related_name='orders')
+class Supplier(models.Model):
+    full_name = models.CharField(max_length=255)
+    contact_person = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
 
     def __str__(self):
-        return f"Order {self.order_id} by {self.customer}"
-
-
+        return self.full_name
+    
 class Customer(models.Model):
     full_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15)
@@ -62,27 +40,51 @@ class Customer(models.Model):
     def __str__(self):
         return self.full_name
 
-
-class Supplier(models.Model):
-    full_name = models.CharField(max_length=255)
-    contact_person = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=15)
+class PriceHistory(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='price_history_item')
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name='price_history_size')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    effective_date = models.DateField()
 
     def __str__(self):
-        return self.full_name
+        return f"{self.price} on {self.effective_date}"
+
+
+class IncomingItems(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='incoming_items')
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name='incoming_items_size')
+    quantity_received = models.PositiveIntegerField()
+    date_received = models.DateField()
+    supplier = models.ForeignKey(Supplier,on_delete=models.CASCADE, related_name='supplier')
+
+    def __str__(self):
+        return f"{self.quantity_received}"
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='orders_item')
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name='orders_item_size')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders_customer')
+    quantity = models.PositiveIntegerField()
+    price = models.ForeignKey(PriceHistory, on_delete=models.CASCADE, related_name='orders_price')
+    status = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"Order {self.order_id} by {self.customer}"
 
 
 class Status(models.Model):
-    description = models.CharField(max_length=50)
+    status_name = models.CharField(max_length=150, unique=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.description
 
 
 class Users(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE, related_name='users')
-    username = models.CharField(max_length=150, unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, related_name='status')
+    
 
     def __str__(self):
         return self.username
